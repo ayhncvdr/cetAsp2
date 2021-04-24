4a4a59539c4d13926465180f1c939aa3655393cb
ayhncvdr <73463728+ayhncvdr@users.noreply.github.com>
2021-04-24 20:39:42 +0300

    Add project files.

---
+++ cetAsp2/Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using cetAsp2.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace cetAsp2.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Student> Students { get; set; }
+
+
+    }
+}
---
+++ cetAsp2/Models/Student.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace cetAsp2.Models
+{
+    public class Student
+    {
+        public Student()
+        {
+            
+            
+            
+
+            
+        }
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="Please enter a name!")]
+
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Please enter a surname!")]
+        public string Surname { get; set; }
+
+        public string Department { get; set; }
+
+        [Required(ErrorMessage = "Please enter a birth date!")]
+
+        [DataType(DataType.Date)]
+        public DateTime BirthDate { get; set; }
+        public decimal Height { get; set; }
+        public decimal Weight { get; set; }
+
+        public DateTime CreationDate { get; set; }
+
+        
+        public DateTime LastUpdateDate { get; set; }
+        public int Age { get ; set; }
+
+
+
+
+
+
+
+    }
+}
